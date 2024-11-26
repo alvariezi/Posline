691cbb921b06272032f565f18c39ff3270a3bb51
wildandaffa15 <161112149+wildandaffa15@users.noreply.github.com>
2024-11-26 21:39:34 +0700

    set up

---
+++ src/app/components/HomePage.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+const HomePage = () => {
+  return ( 
+    <div className="">Home Page</div>
+   );
+}
+ 
+export default HomePage;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import HomePage from "./components/HomePage";
+
+const Home = async () => {
+  return (
+    <main>
+      <HomePage />
+    </main>
+  );
+};
+
+export default Home;
