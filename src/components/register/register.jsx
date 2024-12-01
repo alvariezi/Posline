e4ba595a1b46a49ea4adfb4ce1f16aff843af019
wildandaffa15 <161112149+wildandaffa15@users.noreply.github.com>
2024-12-01 23:09:30 +0700

    add product-page & settings-page

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,18 +1,24 @@
-"use client"; 
+"use client";
 
 import { useState } from "react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation"; 
 
-const LoginForm = () => {
+const RegisterForm = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const router = useRouter(); 
+
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const navigateToRegister = () => {
-    router.push("/register");  
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const navigateTologin = () => {
+    router.push("/login-page");  
   };
 
   return (
@@ -21,7 +27,7 @@ const LoginForm = () => {
       <div className="md:w-[50%] bg-blue-500 lg:flex md:flex justify-center items-center p-4 hidden">
         <img
           src="/logn&regist.png"
-          alt="Login"
+          alt="Register"
           className="w-full max-w-[420px] h-auto"
         />
       </div>
@@ -32,7 +38,7 @@ const LoginForm = () => {
           tePOS
         </h1>
         <p className="mb-[10px] text-[16px] md:text-[18px] font-bold text-center">
-          Hi, Welcome Back to Flowpos!
+          Create your account
         </p>
         <form className="space-y-4 mx-auto max-w-[400px] w-full">
           <div>
@@ -41,6 +47,16 @@ const LoginForm = () => {
               id="username"
               name="username"
               placeholder="Username"
+              required
+              className="mt-1 block w-full p-[12px] border font-[400] text-[#000] border-[#ACACAC] rounded-[10px]"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
               required
               className="mt-1 block w-full p-[12px] border font-[400] text-[#000] border-[#ACACAC] rounded-[10px]"
             />
@@ -66,21 +82,42 @@ const LoginForm = () => {
               )}
             </button>
           </div>
+          <div className="relative">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              required
+              className="mt-1 block w-full mb-[20px] p-[12px] border font-[400] text-[#000] border-[#ACACAC] rounded-[10px]"
+            />
+            <button
+              type="button"
+              onClick={toggleConfirmPasswordVisibility}
+              className="absolute inset-y-0 right-0 flex items-center pr-3"
+            >
+              {showConfirmPassword ? (
+                <EyeSlashIcon className="h-5 w-5 text-gray-500" />
+              ) : (
+                <EyeIcon className="h-5 w-5 text-gray-500" />
+              )}
+            </button>
+          </div>
           <button
             type="submit"
             className="w-full bg-[#205FFF] text-white font-[600] text-[15px] p-[8px] rounded-[10px] hover:bg-blue-700 transition-all duration-300 ease-in-out"
           >
-            Login
+            Register
           </button>
           <p className="mt-4 text-[14px] font-[600] text-[#000000]">
-            Dont have an account?
+            Already have an account?
           </p>
           <button
             type="button"
-            onClick={navigateToRegister} 
+            onClick={navigateTologin} 
             className="w-full bg-[#000000] text-white font-[600] text-[15px] p-[8px] rounded-[10px] hover:bg-gray-700 transition-all duration-300 ease-in-out border-[2px]"
           >
-            Register
+            Login
           </button>
         </form>
       </div>
@@ -88,4 +125,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
